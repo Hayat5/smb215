4029b77312ec98e51f447d8ae683e94493095c27
Hayat Bourgi <hayat.bourgi@gmail.com>
2015-11-15 11:38:31 +0200

    update GestionDesBiens

--- GestionDesBiens/basededonnee/GestionBiens.sql
+++ GestionDesBiens/basededonnee/GestionBiens.sql
@@ -1,20 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
+-- version 4.4.10
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 14, 2015 at 08:19 PM
--- Server version: 5.5.44-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.11
+-- Generation Time: Nov 15, 2015 at 10:36 AM
+-- Server version: 5.5.42
+-- PHP Version: 5.6.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `GestionBiens`
@@ -26,19 +20,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `center`
 --
 
-CREATE TABLE IF NOT EXISTS `center` (
-  `center_id` int(11) NOT NULL AUTO_INCREMENT,
-  `center_name` varchar(50) NOT NULL,
-  PRIMARY KEY (`center_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+CREATE TABLE `center` (
+  `center_id` int(11) NOT NULL,
+  `center_name` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `center`
---
 
-INSERT INTO `center` (`center_id`, `center_name`) VALUES
-(1, 'Beirut'),
-(4, 'Bikfaya');
 
 -- --------------------------------------------------------
 
@@ -46,25 +33,14 @@ INSERT INTO `center` (`center_id`, `center_name`) VALUES
 -- Table structure for table `groupe`
 --
 
-CREATE TABLE IF NOT EXISTS `groupe` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `groupe` (
+  `id` int(11) NOT NULL,
   `username` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
   `groupname` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
-  `Description` text COLLATE utf8_unicode_ci,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username_2` (`username`,`groupname`),
-  UNIQUE KEY `username_3` (`username`,`groupname`),
-  KEY `username` (`username`,`groupname`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
+  `Description` text COLLATE utf8_unicode_ci
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
---
--- Dumping data for table `groupe`
---
 
-INSERT INTO `groupe` (`id`, `username`, `groupname`, `Description`) VALUES
-(1, 'admin', 'admin', NULL),
-(2, 'Hayat', 'admin', NULL),
-(3, 'user', 'user', NULL);
 
 -- --------------------------------------------------------
 
@@ -72,25 +48,17 @@ INSERT INTO `groupe` (`id`, `username`, `groupname`, `Description`) VALUES
 -- Table structure for table `item`
 --
 
-CREATE TABLE IF NOT EXISTS `item` (
+CREATE TABLE `item` (
   `item_id` int(11) NOT NULL,
   `item_code` varchar(255) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
   `item_date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `item_name` varchar(255) NOT NULL,
   `type_id` int(11) NOT NULL,
   `item_specification` text NOT NULL,
-  `location_id` int(11) NOT NULL,
-  PRIMARY KEY (`item_id`),
-  KEY `item_type` (`type_id`),
-  KEY `location_id` (`location_id`)
+  `location_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `item`
---
 
-INSERT INTO `item` (`item_id`, `item_code`, `item_date_created`, `item_name`, `type_id`, `item_specification`, `location_id`) VALUES
-(1, '584042-001', '2015-10-13 14:49:01', 'Notebook', 2, 'HP pavilion dv3', 1);
 
 -- --------------------------------------------------------
 
@@ -98,23 +66,13 @@ INSERT INTO `item` (`item_id`, `item_code`, `item_date_created`, `item_name`, `t
 -- Table structure for table `location`
 --
 
-CREATE TABLE IF NOT EXISTS `location` (
-  `location_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `location` (
+  `location_id` int(11) NOT NULL,
   `center_id` int(11) NOT NULL,
   `salle_id` int(11) NOT NULL,
-  `personnel_id` int(11) NOT NULL,
-  PRIMARY KEY (`location_id`),
-  KEY `center_id` (`center_id`,`salle_id`,`personnel_id`),
-  KEY `personnel_id` (`personnel_id`),
-  KEY `salle_id` (`salle_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+  `personnel_id` int(11) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Dumping data for table `location`
---
-
-INSERT INTO `location` (`location_id`, `center_id`, `salle_id`, `personnel_id`) VALUES
-(1, 1, 2, 1);
 
 -- --------------------------------------------------------
 
@@ -122,18 +80,12 @@ INSERT INTO `location` (`location_id`, `center_id`, `salle_id`, `personnel_id`) 
 -- Table structure for table `personnel`
 --
 
-CREATE TABLE IF NOT EXISTS `personnel` (
-  `personnel_id` int(11) NOT NULL AUTO_INCREMENT,
-  `personnel_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`personnel_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=2 ;
+CREATE TABLE `personnel` (
+  `personnel_id` int(11) NOT NULL,
+  `personnel_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
---
--- Dumping data for table `personnel`
---
 
-INSERT INTO `personnel` (`personnel_id`, `personnel_name`) VALUES
-(1, 'Dr Pascal Fares');
 
 -- --------------------------------------------------------
 
@@ -141,19 +93,11 @@ INSERT INTO `personnel` (`personnel_id`, `personnel_name`) VALUES
 -- Table structure for table `salle`
 --
 
-CREATE TABLE IF NOT EXISTS `salle` (
-  `salle_id` int(11) NOT NULL AUTO_INCREMENT,
-  `salle_name` varchar(50) NOT NULL,
-  PRIMARY KEY (`salle_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+CREATE TABLE `salle` (
+  `salle_id` int(11) NOT NULL,
+  `salle_name` varchar(50) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `salle`
---
-
-INSERT INTO `salle` (`salle_id`, `salle_name`) VALUES
-(1, 'Beirut'),
-(2, 'Bikfaya');
 
 -- --------------------------------------------------------
 
@@ -161,25 +105,17 @@ INSERT INTO `salle` (`salle_id`, `salle_name`) VALUES
 -- Table structure for table `transaction`
 --
 
-CREATE TABLE IF NOT EXISTS `transaction` (
-  `transaction_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `transaction` (
+  `transaction_id` int(11) NOT NULL,
   `item_id` int(11) NOT NULL,
   `transaction_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `location_id_src` int(11) NOT NULL,
   `location_id_now` int(11) DEFAULT NULL,
   `location_id_dest` int(11) DEFAULT NULL,
-  `transport_id` int(11) DEFAULT NULL,
-  `status` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (`transaction_id`),
-  KEY `transaction_item` (`item_id`),
-  KEY `transaction_user` (`username`),
-  KEY `transport_id` (`transport_id`),
-  KEY `item_id` (`item_id`,`username`,`location_id_src`,`location_id_now`,`location_id_dest`),
-  KEY `location_id_src` (`location_id_src`),
-  KEY `location_id_now` (`location_id_now`),
-  KEY `location_id_dest` (`location_id_dest`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `transport_id` int(11) DEFAULT '0',
+  `status` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -187,13 +123,13 @@ CREATE TABLE IF NOT EXISTS `transaction` (
 -- Table structure for table `transport`
 --
 
-CREATE TABLE IF NOT EXISTS `transport` (
-  `transport_id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `transport` (
+  `transport_id` int(11) NOT NULL,
   `personnel_id` int(11) NOT NULL,
-  `transport_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`transport_id`),
-  KEY `personnel_id` (`personnel_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
+  `transport_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+
 
 -- --------------------------------------------------------
 
@@ -201,19 +137,12 @@ CREATE TABLE IF NOT EXISTS `transport` (
 -- Table structure for table `type`
 --
 
-CREATE TABLE IF NOT EXISTS `type` (
-  `type_id` int(11) NOT NULL AUTO_INCREMENT,
-  `type_name` varchar(255) NOT NULL,
-  PRIMARY KEY (`type_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+CREATE TABLE `type` (
+  `type_id` int(11) NOT NULL,
+  `type_name` varchar(255) NOT NULL
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
 
---
--- Dumping data for table `type`
---
 
-INSERT INTO `type` (`type_id`, `type_name`) VALUES
-(1, 'Consommable'),
-(2, 'A long terme');
 
 -- --------------------------------------------------------
 
@@ -221,12 +150,11 @@ INSERT INTO `type` (`type_id`, `type_name`) VALUES
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE `users` (
   `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `username` varchar(20) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `register_dt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`username`)
+  `register_dt` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
@@ -234,9 +162,133 @@ CREATE TABLE IF NOT EXISTS `users` (
 --
 
 INSERT INTO `users` (`name`, `username`, `password`, `register_dt`) VALUES
-('Guitta', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '0000-00-00 00:00:00');
-('Hayat', 'Hayat', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '0000-00-00 00:00:00');
+('Hayat', 'admin', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', '2015-10-18 13:13:18'),
+('hay', 'hay', '4e30ea25cda1c18cfc423e4d9dfa57b7051a9ef311e09ed43f8e33523f3d9533', '2015-11-19 07:11:17'),
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `center`
+--
+ALTER TABLE `center`
+  ADD PRIMARY KEY (`center_id`);
+
+--
+-- Indexes for table `groupe`
+--
+ALTER TABLE `groupe`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username_2` (`username`,`groupname`),
+  ADD UNIQUE KEY `username_3` (`username`,`groupname`),
+  ADD KEY `username` (`username`,`groupname`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`item_id`),
+  ADD KEY `item_type` (`type_id`),
+  ADD KEY `location_id` (`location_id`);
+
+--
+-- Indexes for table `location`
+--
+ALTER TABLE `location`
+  ADD PRIMARY KEY (`location_id`),
+  ADD KEY `center_id` (`center_id`,`salle_id`,`personnel_id`),
+  ADD KEY `personnel_id` (`personnel_id`),
+  ADD KEY `salle_id` (`salle_id`);
+
+--
+-- Indexes for table `personnel`
+--
+ALTER TABLE `personnel`
+  ADD PRIMARY KEY (`personnel_id`);
+
+--
+-- Indexes for table `salle`
+--
+ALTER TABLE `salle`
+  ADD PRIMARY KEY (`salle_id`);
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`transaction_id`),
+  ADD KEY `transaction_item` (`item_id`),
+  ADD KEY `transaction_user` (`username`),
+  ADD KEY `transport_id` (`transport_id`),
+  ADD KEY `item_id` (`item_id`,`username`,`location_id_src`,`location_id_now`,`location_id_dest`),
+  ADD KEY `location_id_src` (`location_id_src`),
+  ADD KEY `location_id_now` (`location_id_now`),
+  ADD KEY `location_id_dest` (`location_id_dest`);
+
+--
+-- Indexes for table `transport`
+--
+ALTER TABLE `transport`
+  ADD PRIMARY KEY (`transport_id`),
+  ADD KEY `personnel_id` (`personnel_id`);
+
+--
+-- Indexes for table `type`
+--
+ALTER TABLE `type`
+  ADD PRIMARY KEY (`type_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `center`
+--
+ALTER TABLE `center`
+  MODIFY `center_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=24;
+--
+-- AUTO_INCREMENT for table `groupe`
+--
+ALTER TABLE `groupe`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
+--
+-- AUTO_INCREMENT for table `location`
+--
+ALTER TABLE `location`
+  MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `personnel`
+--
+ALTER TABLE `personnel`
+  MODIFY `personnel_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `salle`
+--
+ALTER TABLE `salle`
+  MODIFY `salle_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=22;
+--
+-- AUTO_INCREMENT for table `transport`
+--
+ALTER TABLE `transport`
+  MODIFY `transport_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT for table `type`
+--
+ALTER TABLE `type`
+  MODIFY `type_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=7;
 --
 -- Constraints for dumped tables
 --
@@ -278,7 +330,3 @@ ALTER TABLE `transport`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`username`) REFERENCES `groupe` (`username`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
